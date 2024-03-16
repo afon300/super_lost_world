f991112530bf92b5339ffa7145882e396b9273d0
afon30 <antoinefourneyron@gmail.com>
2024-03-16 02:46:25 +0100

    modifications des images du menu

--- main.py
+++ main.py
@@ -19,25 +19,17 @@ font = pygame.font.SysFont("arialblack", 40)
 
 TEXT_COL = (255, 255, 255)
 
-#load button images
-button_play = os.path.join(os.getcwd(), "button", "button_play.png")
-button_option = os.path.join(os.getcwd(), "button", "button_option.png")
-button_quit = os.path.join(os.getcwd(), "button", "button_play.png")
-button_video = os.path.join(os.getcwd(), "button", "button_play.png")
-button_audio = os.path.join(os.getcwd(), "button", "button_play.png")
-button_keys = os.path.join(os.getcwd(), "button", "button_play.png")
-button_back = os.path.join(os.getcwd(), "button", "button_play.png")
+################################# IMAGES BOUTTONS MENU #################################
 
-resume_img = pygame.image.load(button_play).convert_alpha()
-options_img = pygame.image.load(button_option).convert_alpha()
-quit_img = pygame.image.load(button_quit).convert_alpha()
-video_img = pygame.image.load(button_video).convert_alpha()
-audio_img = pygame.image.load(button_audio).convert_alpha()
-keys_img = pygame.image.load(button_keys).convert_alpha()
-back_img = pygame.image.load(button_back).convert_alpha()
+resume_img = pygame.image.load("button/button_play.png").convert_alpha()
+options_img = pygame.image.load("button/button_option.png").convert_alpha()
+quit_img = pygame.image.load("button/button_quit.png").convert_alpha()
+video_img = pygame.image.load("button/button_video.png").convert_alpha()
+audio_img = pygame.image.load("button/button_audio.png").convert_alpha()
+keys_img = pygame.image.load("button/button_keys.png").convert_alpha()
+back_img = pygame.image.load("button/button_back.png").convert_alpha()
 print(resume_img)
 
-#create button instances
 resume_button = button.Button(304, 125, resume_img, 1)
 options_button = button.Button(297, 250, options_img, 1)
 quit_button = button.Button(336, 375, quit_img, 1)
@@ -50,26 +42,24 @@ def draw_text(text, font, text_col, x, y):
   img = font.render(text, True, text_col)
   screen.blit(img, (x, y))
 
-#game loop
+
+################################## BOUCLE PRINCIPALE ###################################
+
 run = True
 while run:
 
   screen.fill((52, 78, 91))
 
-  #check if game is paused
+
   if game_paused == True:
-    #check menu state
     if menu_state == "main":
-      #draw pause screen buttons
       if resume_button.draw(screen):
         game_paused = False
       if options_button.draw(screen):
         menu_state = "options"
       if quit_button.draw(screen):
         run = False
-    #check if the options menu is open
     if menu_state == "options":
-      #draw the different options buttons
       if video_button.draw(screen):
         print("Video Settings")
       if audio_button.draw(screen):
