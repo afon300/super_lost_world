7dfc87683efb43282f9357be0b65a81fd7ccf63b
afon30 <antoinefourneyron@gmail.com>
2024-03-18 14:24:26 +0100

    menu presque finit

--- button.py
+++ button.py
@@ -1,26 +1,33 @@
 import pygame
 
 class Button():
-	def __init__(self, x, y, image, scale):
-		width = image.get_width()
-		height = image.get_height()
-		self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
-		self.rect = self.image.get_rect()
-		self.rect.topleft = (x, y)
-		self.clicked = False
+    def __init__(self, x, y, image, scale):
+        self.original_image = image
+        width = image.get_width()
+        height = image.get_height()
+        self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+        self.clicked = False
 
-	def draw(self, surface):
-		action = False
-		pos = pygame.mouse.get_pos()
+    def draw(self, surface):
+        action = False
 
-		if self.rect.collidepoint(pos):
-			if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
-				self.clicked = True
-				action = True
+        pos = pygame.mouse.get_pos()
 
-		if pygame.mouse.get_pressed()[0] == 0:
-			self.clicked = False
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and not self.clicked:
+                self.clicked = True
+                action = True
 
-		surface.blit(self.image, (self.rect.x, self.rect.y))
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
 
-		return action
+        surface.blit(self.image, (self.rect.x, self.rect.y))
+
+        return action
+
+    def resize_image(self, scale):
+        width = self.original_image.get_width()
+        height = self.original_image.get_height()
+        self.image = pygame.transform.scale(self.original_image, (int(width * scale), int(height * scale)))
