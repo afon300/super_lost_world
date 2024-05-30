88e9f109c72b418a35faa1febdfddb39c1055136
afon30 <antoinefourneyron@gmail.com>
2024-05-31 01:53:17 +0200

    pre-alpha 0.16

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ pygame.init()
 # Définir les paramètres de la fenêtre
 WINDOW_SIZE, WINDOW_HEIGHT, WINDOW_WIDTH = resolutions_ecran()
 pygame.display.set_caption("Super lost world")
-#pygame.display.set_icon(x)
 screen = pygame.display.set_mode(WINDOW_SIZE)
 pygame.display.set_caption("Main Menu")
 state_main_menu = True
@@ -70,7 +69,6 @@ def draw_text(text, font, text_col, x, y):
 
 ###################################### Experimental #######################################
 
-
 CHARACTER_SIZE = (64, 64)
 walk_down = [load_and_scale_image('main_character/marche/kadhafi_face_1.png', CHARACTER_SIZE), 
              load_and_scale_image('main_character/marche/kadhafi_face_2.png', CHARACTER_SIZE), 
@@ -86,45 +84,65 @@ walk_right = [load_and_scale_image('main_character/marche/kadhafi_droit_1.png', 
               load_and_scale_image('main_character/marche/kadhafi_droit_2.png', CHARACTER_SIZE), 
               load_and_scale_image('main_character/marche/kadhafi_droit_3.png', CHARACTER_SIZE),
               load_and_scale_image('main_character/marche/kadhafi_droit_4.png', CHARACTER_SIZE)]
-kadhafi_animations = {
+attack_down = [load_and_scale_image('main_character/attaque/kadhafi_face_1.png', CHARACTER_SIZE), 
+             load_and_scale_image('main_character/attaque/kadhafi_face_2.png', CHARACTER_SIZE), 
+             load_and_scale_image('main_character/attaque/kadhafi_face_3.png', CHARACTER_SIZE)]
+attack_up = [load_and_scale_image('main_character/attaque/kadhafi_dos_1.png', CHARACTER_SIZE), 
+           load_and_scale_image('main_character/attaque/kadhafi_dos_2.png', CHARACTER_SIZE), 
+           load_and_scale_image('main_character/attaque/kadhafi_dos_3.png', CHARACTER_SIZE)]
+attack_left = [load_and_scale_image('main_character/attaque/kadhafi_gauche_1.png', CHARACTER_SIZE), 
+             load_and_scale_image('main_character/attaque/kadhafi_gauche_2.png', CHARACTER_SIZE), 
+             load_and_scale_image('main_character/attaque/kadhafi_gauche_3.png', CHARACTER_SIZE)]
+attack_right = [load_and_scale_image('main_character/attaque/kadhafi_droit_1.png', CHARACTER_SIZE), 
+              load_and_scale_image('main_character/attaque/kadhafi_droit_2.png', CHARACTER_SIZE), 
+              load_and_scale_image('main_character/attaque/kadhafi_droit_3.png', CHARACTER_SIZE)]
+
+kadhafi_walk_animations = {
     'down': walk_down,
     'up': walk_up,
     'left': walk_left,
     'right': walk_right
 }
 
+kadhafi_attack_animations = {
+    'down': attack_down,
+    'up': attack_up,
+    'left': attack_left,
+    'right': attack_right
+}
+
 BLOB_SIZE = (32, 32)
 blob_animation = [load_and_scale_image('PNJ/Mobs/blob/blob_1.png', CHARACTER_SIZE),
                    load_and_scale_image('PNJ/Mobs/blob/blob_2.png', CHARACTER_SIZE),
                    load_and_scale_image('PNJ/Mobs/blob/blob_3.png', CHARACTER_SIZE)]
-    
+
 character_x = 300
 character_y = 200
 
 blob_x = random.randint(100, 600)
 blob_y = random.randint(100, 600)
+blob_health = 2
 clock = pygame.time.Clock()
 
-
 frame_rate = 10
-
 frame = 0
 character_speed = 4
 direction = "down"
 is_moving = False
+is_attack = False
+is_attacking = False 
 
 blob_frame = 0
 blob_speed = 3
 blob_direction = "down"
 blob_is_moving = False
-
+blob_alive = True
 
 exemple_map = pygame.image.load("exemple.png").convert_alpha()
 exemple_map_img = pygame.transform.scale(exemple_map, MENU_SIZE)
 
 keyboard = True
 controller = False
-
 
 if pygame.joystick.get_count() > 0:
     controller = True
@@ -136,9 +154,10 @@ else:
 
 last_press = 0
 input_delay = 750
+last_hit_time = 0
 
 def handle_joystick_events():
-    global character_x, character_y, direction, is_moving, last_press, frame
+    global character_x, character_y, direction, is_moving, last_press, frame, is_attack, is_attacking
 
     if controller:
         x_axis = joystick.get_axis(0)
@@ -149,6 +168,8 @@ def handle_joystick_events():
         if last_press >= input_delay:
             if joystick.get_button(0):
                 print("A")
+                is_attack = True
+                is_attacking = True
                 is_moving = False
                 frame = 0
             if joystick.get_button(1):
@@ -181,23 +202,23 @@ def handle_joystick_events():
             frame = 0
             
 def handle_keyboard_input():
-    global character_x, character_y, direction, is_moving, last_press, frame
+    global character_x, character_y, direction, is_moving, last_press, frame, is_attack, last_tick
 
     keys = pygame.key.get_pressed()
+    current_time = pygame.time.get_ticks()
     if keys[pygame.K_a] or keys[pygame.K_z]:
-        if last_press >= input_delay:
+        if current_time - last_press >= input_delay:
             if keys[pygame.K_a]:
                 print("A")
+                is_attack = True
                 is_moving = False
                 frame = 0
             if keys[pygame.K_z]:
                 print("Z")
                 is_moving = False
                 frame = 0
-            last_press = 0
-        else:
-            last_press += pygame.time.get_ticks() - last_tick
-    if (keys[pygame.K_a] != True) and (keys[pygame.K_z] != True):
+            last_press = current_time
+    else:
         if keys[pygame.K_LEFT]:
             character_x -= character_speed
             direction = 'left'
@@ -217,32 +238,46 @@ def handle_keyboard_input():
         else:
             is_moving = False
             frame = 0
-    else:
-        is_moving = False
-        frame = 0
 
 def handle_input():
-    global character_x, character_y, direction, is_moving, frame, image
     if controller:
         handle_joystick_events()
     if keyboard:
         handle_keyboard_input()
 
-# def annimations_kadhafi(frame, frame_rate):
-#     if is_moving:
-#         frame += 1
-#     if frame >= len(kadhafi_animations[direction]) * frame_rate:
-#         frame = 0
-#     image = kadhafi_animations[direction][frame // frame_rate]
-#     screen.blit(image, (character_x, character_y))
-        
-#     screen.blit(image, (character_x, character_y))
+def check_collision(x1, y1, size1, x2, y2, size2):
+    return (x1 < x2 + size2[0] and x1 + size1[0] > x2 and 
+            y1 < y2 + size2[1] and y1 + size1[1] > y2)
 
-is_alive = True
+def handle_blob_movement():
+    global blob_x, blob_y, blob_direction, blob_is_moving, blob_frame
+
+    if random.randint(1, 20) == 1:
+        direction = random.choice(['left', 'right', 'up', 'down'])
+        blob_direction = direction
+        blob_is_moving = True
+
+    if blob_is_moving:
+        if blob_direction == 'left':
+            blob_x -= blob_speed
+        elif blob_direction == 'right':
+            blob_x += blob_speed
+        elif blob_direction == 'up':
+            blob_y -= blob_speed
+        elif blob_direction == 'down':
+            blob_y += blob_speed
+
+    if blob_x < 0:
+        blob_x = 0
+    elif blob_x > WINDOW_WIDTH - BLOB_SIZE[0]:
+        blob_x = WINDOW_WIDTH - BLOB_SIZE[0]
+
+    if blob_y < 0:
+        blob_y = 0
+    elif blob_y > WINDOW_HEIGHT - BLOB_SIZE[1]:
+        blob_y = WINDOW_HEIGHT - BLOB_SIZE[1]
 
 world_map = False
-       
-################################## BOUCLE PRINCIPALE ###################################
 
 run = True
 while run:
@@ -281,15 +316,47 @@ while run:
         screen.blit(exemple_map_img, (0, 0))
         
         handle_input()
-        if is_moving:
-            frame += 1
-        if frame >= len(kadhafi_animations[direction]) * frame_rate:
-            frame = 0
-        image = kadhafi_animations[direction][frame // frame_rate]
-        screen.blit(image, (character_x, character_y))
+        handle_blob_movement()
         
-        blob_img = blob_animation[blob_frame // frame_rate]
-        screen.blit(blob_img, (blob_x, blob_y))
+        # Animation de marche
+        if is_moving and not is_attack:
+            frame += 1
+            if frame >= len(kadhafi_walk_animations[direction]) * frame_rate:
+                frame = 0
+            image = kadhafi_walk_animations[direction][frame // frame_rate]
+            screen.blit(image, (character_x, character_y))
+
+        # Animation d'attaque
+        if is_attack:
+            frame += 1
+            if frame >= len(kadhafi_attack_animations[direction]) * frame_rate:
+                frame = 0
+                is_attack = False
+            image = kadhafi_attack_animations[direction][frame // frame_rate]
+            screen.blit(image, (character_x, character_y))
+
+            # Collision de l'attaque avec le blob
+            current_time = pygame.time.get_ticks()
+            if check_collision(character_x, character_y, CHARACTER_SIZE, blob_x, blob_y, BLOB_SIZE):
+                if current_time - last_hit_time >= 500:
+                    blob_health -= 1
+                    last_hit_time = current_time
+                    if blob_health <= 0:
+                        blob_alive = False
+                    print(f"Blob health: {blob_health}")
+
+        # Si aucune animation n'est en cours, afficher l'image par défaut
+        if not is_moving and not is_attack:
+            image = kadhafi_walk_animations[direction][0]
+            screen.blit(image, (character_x, character_y))
+        
+        if blob_alive:
+            blob_frame += 1
+            if blob_frame >= len(blob_animation) * frame_rate:
+                blob_frame = 0
+            blob_img = blob_animation[blob_frame // frame_rate]
+            screen.blit(blob_img, (blob_x, blob_y))
+
     pygame.display.flip()
     clock.tick(60)
 
