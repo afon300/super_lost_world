0452943d7b2cf9092f6a839105960aff86e3d40b
afon30 <antoinefourneyron@gmail.com>
2024-06-20 02:00:41 +0200

    ajout des dernières fonctionnalités

--- main.py
+++ main.py
@@ -116,12 +116,23 @@ blob_animation = [load_and_scale_image('PNJ/Mobs/blob/blob_1.png', CHARACTER_SIZ
                    load_and_scale_image('PNJ/Mobs/blob/blob_2.png', CHARACTER_SIZE),
                    load_and_scale_image('PNJ/Mobs/blob/blob_3.png', CHARACTER_SIZE)]
 
+FOUDRE_SIZE = (32, 32)
+foudre_animation = [load_and_scale_image('PNJ/Mobs/monstre_de_foudre/foudre_1.png', CHARACTER_SIZE),
+                   load_and_scale_image('PNJ/Mobs/monstre_de_foudre/foudre_2.png', CHARACTER_SIZE),
+                   load_and_scale_image('PNJ/Mobs/monstre_de_foudre/foudre_3.png', CHARACTER_SIZE)]
+
 character_x = 300
 character_y = 200
 
 blob_x = random.randint(100, 600)
 blob_y = random.randint(100, 600)
+
+foudre_x = random.randint(100, 600)
+foudre_y = random.randint(100, 600)
+
 blob_health = 2
+foudre_health = 4
+
 clock = pygame.time.Clock()
 
 frame_rate = 10
@@ -133,13 +144,21 @@ is_attack = False
 is_attacking = False 
 
 blob_frame = 0
-blob_speed = 3
+blob_speed = 1
 blob_direction = "down"
 blob_is_moving = False
 blob_alive = True
 
-exemple_map = pygame.image.load("exemple.png").convert_alpha()
-exemple_map_img = pygame.transform.scale(exemple_map, MENU_SIZE)
+foudre_frame = 0
+foudre_speed = 2
+foudre_direction = "down"
+foudre_is_moving = False
+foudre_alive = True
+
+background_map = pygame.image.load("background.bmp")
+background_map_img = pygame.transform.scale(background_map, MENU_SIZE)
+collision_map = pygame.image.load("collision_img.bmp")
+collision_map_img = pygame.transform.scale(collision_map, MENU_SIZE)
 
 keyboard = True
 controller = False
@@ -156,6 +175,44 @@ last_press = 0
 input_delay = 750
 last_hit_time = 0
 
+def change_map(new_background_path, new_collision_path):
+    global background_map, background_map_img, collision_map, collision_map_img, character_x, character_y
+
+    # Vérifier si la position actuelle du joueur touche la couleur bleue
+    current_player_x, current_player_y = character_x, character_y
+    if collision_map_img.get_at((current_player_x, current_player_y)) == (0, 0, 255):
+        # Charger les nouvelles images de fond et de collision
+        background_map = pygame.image.load(new_background_path)
+        background_map_img = pygame.transform.scale(background_map, MENU_SIZE)
+
+        collision_map = pygame.image.load(new_collision_path)
+        collision_map_img = pygame.transform.scale(collision_map, MENU_SIZE)
+
+        character_x, character_y = start_x, start_y
+
+
+actual_map = 1
+
+def check_player_position():
+    global actual_map, start_x, start_y
+
+    current_player_x, current_player_y = character_x, character_y
+    if collision_map_img.get_at((current_player_x, current_player_y)) == (0, 0, 255):
+        if actual_map == 1 and current_player_x < 10:  # Exemple de condition pour la zone bleue sur la carte 1
+            start_x = 1100
+            start_y = 450
+            change_map("background_2.bmp", "collision_img_2.bmp")
+            actual_map = 2
+        elif actual_map == 2 and current_player_x > WINDOW_WIDTH - 10:  # Exemple de condition pour la zone bleue sur la carte 2
+            start_x = 50
+            start_y = 450
+            change_map("background.bmp", "collision_img.bmp")
+            actual_map = 1
+
+
+
+
+
 def handle_joystick_events():
     global character_x, character_y, direction, is_moving, last_press, frame, is_attack, is_attacking, last_tick
 
@@ -163,44 +220,47 @@ def handle_joystick_events():
         x_axis = joystick.get_axis(0)
         y_axis = joystick.get_axis(1)
         threshold = 0.1
-    
-    if joystick.get_button(0) or joystick.get_button(1):
-        if last_press >= input_delay:
-            if joystick.get_button(0):
-                print("A")
-                is_attack = True
-                is_attacking = True
+
+        if joystick.get_button(0) or joystick.get_button(1):
+            if last_press >= input_delay:
+                if joystick.get_button(0):
+                    print("A")
+                    is_attack = True
+                    is_attacking = True
+                    is_moving = False
+                    frame = 0
+                if joystick.get_button(1):
+                    print("B")
+                    is_moving = False
+                    frame = 0
+                last_press = 0
+            else:
+                last_press += pygame.time.get_ticks() - last_tick
+        else:
+            if abs(x_axis) > threshold or abs(y_axis) > threshold:
+                new_x, new_y = character_x, character_y
+                if x_axis < -threshold:
+                    new_x -= character_speed
+                    direction = 'left'
+                elif x_axis > threshold:
+                    new_x += character_speed
+                    direction = 'right'
+                if y_axis < -threshold:
+                    new_y -= character_speed
+                    direction = 'up'
+                elif y_axis > threshold:
+                    new_y += character_speed
+                    direction = 'down'
+
+                if not check_collision_with_obstacles(new_x, new_y):
+                    character_x, character_y = new_x, new_y
+                    is_moving = True
+                else:
+                    is_moving = False
+            else:
                 is_moving = False
                 frame = 0
-            if joystick.get_button(1):
-                print("B")
-                is_moving = False
-                frame = 0
-            last_press = 0
-        else:
-            last_press += pygame.time.get_ticks() - last_tick
-    else:
-        if abs(x_axis) > threshold or abs(y_axis) > threshold:
-            if x_axis < -threshold:
-                character_x -= character_speed
-                direction = 'left'
-                is_moving = True
-            elif x_axis > threshold:
-                character_x += character_speed
-                direction = 'right'
-                is_moving = True
-            if y_axis < -threshold:
-                character_y -= character_speed
-                direction = 'up'
-                is_moving = True
-            elif y_axis > threshold:
-                character_y += character_speed
-                direction = 'down'
-                is_moving = True
-        else:
-            is_moving = False
-            frame = 0
-            
+
 def handle_keyboard_input():
     global character_x, character_y, direction, is_moving, last_press, frame, is_attack, is_attacking, last_tick
 
@@ -220,35 +280,32 @@ def handle_keyboard_input():
                 frame = 0
             last_press = current_time
     else:
+        new_x, new_y = character_x, character_y
         if keys[pygame.K_LEFT]:
-            character_x -= character_speed
+            new_x -= character_speed
             direction = 'left'
-            is_moving = True
         elif keys[pygame.K_RIGHT]:
-            character_x += character_speed
+            new_x += character_speed
             direction = 'right'
-            is_moving = True
         elif keys[pygame.K_UP]:
-            character_y -= character_speed
+            new_y -= character_speed
             direction = 'up'
-            is_moving = True
         elif keys[pygame.K_DOWN]:
-            character_y += character_speed
+            new_y += character_speed
             direction = 'down'
+
+        if not check_collision_with_obstacles(new_x, new_y):
+            character_x, character_y = new_x, new_y
             is_moving = True
         else:
             is_moving = False
-            frame = 0
+        frame = 0
 
 def handle_input():
     if controller:
         handle_joystick_events()
     if keyboard:
         handle_keyboard_input()
-
-def check_collision(x1, y1, size1, x2, y2, size2):
-    return (x1 < x2 + size2[0] and x1 + size1[0] > x2 and 
-            y1 < y2 + size2[1] and y1 + size1[1] > y2)
 
 def handle_blob_movement():
     global blob_x, blob_y, blob_direction, blob_is_moving, blob_frame
@@ -277,8 +334,54 @@ def handle_blob_movement():
         blob_y = 0
     elif blob_y > WINDOW_HEIGHT - BLOB_SIZE[1]:
         blob_y = WINDOW_HEIGHT - BLOB_SIZE[1]
+        
+def handle_foudre_movement():
+    global foudre_x, foudre_y, foudre_direction, foudre_is_moving, foudre_frame
 
+    if random.randint(1, 20) == 1:
+        direction = random.choice(['left', 'right', 'up', 'down'])
+        foudre_direction = direction
+        foudre_is_moving = True
+
+    if foudre_is_moving:
+        if foudre_direction == 'left':
+            foudre_x -= foudre_speed
+        elif foudre_direction == 'right':
+            foudre_x += foudre_speed
+        elif foudre_direction == 'up':
+            foudre_y -= foudre_speed
+        elif foudre_direction == 'down':
+            foudre_y += foudre_speed
+
+    if foudre_x < 0:
+        foudre_x = 0
+    elif foudre_x > WINDOW_WIDTH - FOUDRE_SIZE[0]:
+        foudre_x = WINDOW_WIDTH - FOUDRE_SIZE[0]
+
+    if foudre_y < 0:
+        foudre_y = 0
+    elif foudre_y > WINDOW_HEIGHT - FOUDRE_SIZE[1]:
+        foudre_y = WINDOW_HEIGHT - FOUDRE_SIZE[1]
+
+def check_collision(x1, y1, size1, x2, y2, size2):
+    return (x1 < x2 + size2[0] and x1 + size1[0] > x2 and 
+            y1 < y2 + size2[1] and y1 + size1[1] > y2)
+    
+def check_collision_with_obstacles(x, y):
+    collision_color = (163, 73, 164)
+    x = int(x)
+    y = int(y)
+    if 0 <= x < collision_map_img.get_width() and 0 <= y < collision_map_img.get_height():
+        if collision_map_img.get_at((x, y)) == collision_color:
+            return True
+    return False
+
+
+pygame.mixer.music.load("musique_de_fond.mp3")
+pygame.mixer.music.set_volume(0.2)
+music_playing = False 
 world_map = False
+actual_map = 1
 
 run = True
 while run:
@@ -313,11 +416,16 @@ while run:
             if back_button.draw(screen):
                 menu_state = "main"
     if world_map:
+        # if not music_playing:  # Jouer la musique une seule fois
+        #     pygame.mixer.music.play(-1)
+        #     music_playing = True
+            
         screen.fill((0, 0, 0))
-        screen.blit(exemple_map_img, (0, 0))
+        screen.blit(background_map_img, (0, 0))
         
         handle_input()
         handle_blob_movement()
+        handle_foudre_movement()
         
         # marche
         if is_moving and not is_attack:
@@ -332,13 +440,12 @@ while run:
             is_moving = False
             frame += 1
             if frame >= len(kadhafi_attack_animations[direction]) * frame_rate:
-                print(len(kadhafi_attack_animations[direction]) * frame_rate)
                 frame = 0
                 is_attack = False
             image = kadhafi_attack_animations[direction][frame // frame_rate]
             screen.blit(image, (character_x, character_y))
 
-            # Collision
+            # Collision avec le blob
             current_time = pygame.time.get_ticks()
             if check_collision(character_x, character_y, CHARACTER_SIZE, blob_x, blob_y, BLOB_SIZE):
                 if current_time - last_hit_time >= 500:
@@ -347,6 +454,14 @@ while run:
                     if blob_health <= 0:
                         blob_alive = False
                     print(f"Blob health: {blob_health}")
+                    
+            if check_collision(character_x, character_y, CHARACTER_SIZE, foudre_x, foudre_y, FOUDRE_SIZE):
+                if current_time - last_hit_time >= 500:
+                    foudre_health -= 1
+                    last_hit_time = current_time
+                    if foudre_health <= 0:
+                        foudre_alive = False
+                    print(f"foudre health: {foudre_health}")
 
         if not is_moving and not is_attack:
             image = kadhafi_walk_animations[direction][0]
@@ -358,13 +473,20 @@ while run:
                 blob_frame = 0
             blob_img = blob_animation[blob_frame // frame_rate]
             screen.blit(blob_img, (blob_x, blob_y))
-
+            
+        if foudre_alive:
+            foudre_frame += 1
+            if foudre_frame >= len(foudre_animation) * frame_rate:
+                foudre_frame = 0
+            foudre_img = foudre_animation[foudre_frame // frame_rate]
+            screen.blit(foudre_img, (foudre_x, foudre_y))
+        
+        check_player_position()
+        
+        
     pygame.display.flip()
     clock.tick(60)
 
+pygame.mixer.music.stop()
 pygame.quit()
 sys.exit()
-
-
-
-#Notre Jeux vidéo consiste à intégrer un monde de fantaisie au sein du quel l'exploration et l'aventure sont les mots d'ordre, le joueur devras résoudre des enigmes et afronter des ennemies pour dépolluer ce monde dévasté
